69440f462af55336b58c815401b2c8e117b911be
DeepBhupatkar <bhupatkardeep@gmail.com>
2024-08-31 13:31:37 +0530

    Added Effects

--- ItsGlowTimeAppleEventSwiftUI/ItsGlowTimeAppleEventSwiftUI/ContentView.swift
+++ ItsGlowTimeAppleEventSwiftUI/ItsGlowTimeAppleEventSwiftUI/ContentView.swift
@@ -5,36 +5,94 @@
 //  Created by DEEP BHUPATKAR on 31/08/24.
 //
 
+
+
 import SwiftUI
-import CommonSwiftUI
 
 struct ContentView: View {
-    
-    let
-gradient: LinearGradient = .linearGradient(.init(colors:
-                                                    [.blue, .purple, .orange]), startPoint:.leading, endPoint: .trailing)
-    
     var body: some View {
-        VStack {
-            Image(systemName: "apple.logo")
-              
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 200, height: 200)
-                .glow(gradient: gradient, intensity: 5)
-           
-            
-            Text ("It's Glowtime" )
-                .font(system(size: 54))
-                .fontWeight(.semibold)
-                .glow (gradient: gradient, intensity: 5)
-            
-            Text ("September 9, 2024")
-        }
-     
+        GlowingView()
     }
 }
 
 #Preview {
     ContentView()
 }
+
+
+
+struct GlowingView: View {
+    
+    @State private var glow = false
+    
+    let gradient: LinearGradient = LinearGradient(
+        gradient: Gradient(colors: [.blue, .purple, .orange,.pink, .red,.white]),
+        startPoint: .leading,
+        endPoint: .trailing
+    )
+    
+    var body: some View {
+        
+        VStack {
+            
+            Image(systemName: "apple.logo")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 200, height: 200)
+                .foregroundColor(.clear)
+                .overlay(gradient)
+                .mask(
+                    Image(systemName: "apple.logo")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 200, height: 200)
+                )
+                .shadow(color: glow ? .blue.opacity(0.7) : .purple.opacity(0.7), radius: glow ? 30 : 10)
+                .scaleEffect(glow ? 1.05 : 1.0)
+                .animation(
+                    Animation.easeInOut(duration: 2)
+                        .repeatForever(autoreverses: true)
+                )
+                .onAppear {
+                    glow.toggle()
+                }
+            
+                .padding()
+                .padding()
+            
+            Text("It's Glowtime")
+                .font(.system(size: 54))
+                .fontWeight(.semibold)
+                .foregroundColor(.clear)
+                .overlay(gradient)
+                .mask(Text("It's Glowtime")
+                    .font(.system(size: 54))
+                    .fontWeight(.semibold))
+                .shadow(color: glow ? .orange.opacity(0.7) : .purple.opacity(0.7), radius: glow ? 20 : 10)
+                .scaleEffect(glow ? 1.05 : 1.0)
+                .animation(
+                    Animation.easeInOut(duration: 2)
+                        .repeatForever(autoreverses: true)
+                )
+            
+                .padding()
+                .padding()
+            
+            Text("September 9, 2024")
+                .font(.title)
+                .fontWeight(.regular)
+                .foregroundColor(.indigo)
+                .shadow(color: glow ? .orange.opacity(0.7) : .purple.opacity(0.7), radius: glow ? 20 : 10)
+                .scaleEffect(glow ? 1.05 : 1.0)
+                .animation(
+                    Animation.easeInOut(duration: 2)
+                        .repeatForever(autoreverses: true)
+                )
+
+        }
+        
+
+    }
+}
+
+
